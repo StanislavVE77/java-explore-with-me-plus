f31ce31eb2df772069eeac33316b788322dda20d
Zotov Slava <zotovvslava1@mail.ru>
2025-05-26 21:35:07 +0300

    9 проба

--- stats/stats-client/src/main/java/ru/practicum/client/RestClientConfig.java
+++ stats/stats-client/src/main/java/ru/practicum/client/RestClientConfig.java
@@ -1,15 +1,19 @@
 package ru.practicum.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
+    @Value("${stats.server-url}")
+    private String statsServerUrl;
+
     @Bean
     public RestClient restClient() {
         return RestClient.builder()
-                .baseUrl("http://localhost:9090")
+                .baseUrl(statsServerUrl)
                 .build();
     }
 }
